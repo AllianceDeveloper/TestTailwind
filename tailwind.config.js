c224d69951369d5cec6defbd64d511177f5c26f7
Wilbert Alexander Lopez Perez <lopezw.alexander@gmailcom>
2022-09-11 20:18:47 -0700

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./*.css", "./css/*.css"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
